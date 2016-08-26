e37462b0d69529ba33b34b78f3fb099917f245d9
Florin Patan <florinpatan@gmail.com>
2016-08-26 23:53:06 +0100

    gofmt

--- models/event.go
+++ models/event.go
@@ -5,8 +5,8 @@ import "fmt"
 
 // Event event
 type Event struct {
-	Name        string   `json:"name"`
-	Description string   `json:description`
+	Name        string `json:"name"`
+	Description string `json:description`
 	Speakers    []string
 }
 
@@ -14,36 +14,35 @@ type Event struct {
 type Events []Event
 
 /**
- * @TODO Should these functions be methods instead? 
+ * @TODO Should these functions be methods instead?
  */
 
 // Create Event
 func CreateEvent(db *sql.DB, name string, description string, speakers []string) {
-	id,err := db.Exec(
+	id, err := db.Exec(
 		"INSERT INTO events (name, description) OUTPUT INSERTED.id VALUES ($1, $2)",
-	    name,
-	    description,
+		name,
+		description,
 	)
 
-	for _,speaker := range speakers {
-		
+	for _, speaker := range speakers {
+
 		//@TODO check if speaker identifier exists
 
-		_,err := db.Exec(
+		_, err := db.Exec(
 			"INSERT INTO speaker_event (speaker, event) VALUES ($1, $2)",
-		    speaker,
-		    id,
+			speaker,
+			id,
 		)
 
 		if err != nil {
 			fmt.Println("Error creating speaker_event entry %v", err)
-		}	
+		}
 	}
-
 
 	if err != nil {
 		fmt.Println("Error in creating event entry: %v", err)
-		// @TODO Log error 
+		// @TODO Log error
 	}
 }
 
@@ -56,9 +55,9 @@ func CreateEvent(db *sql.DB, name string, description string, speakers []string)
 func UpdateEvent(db *sql.DB, id int, name string, description string) {
 
 	// @TODO figure out how you want to separate only updating one column
-	// 2 transactions? If statement logic? 
+	// 2 transactions? If statement logic?
 
-	_,err := db.Exec(
+	_, err := db.Exec(
 		"UPDATE event SET name=$1, description=$2, WHERE id=$3",
 		name,
 		description,
@@ -67,21 +66,21 @@ func UpdateEvent(db *sql.DB, id int, name string, description string) {
 
 	if err != nil {
 		fmt.Println("Error in updating event entry: %v", err)
-		// @TODO Log error 
+		// @TODO Log error
 	}
 }
 
 // Delete blah
 func DeleteEvent(db *sql.DB, id int) {
 
-	_,err := db.Exec(
+	_, err := db.Exec(
 		"DELETE FROM events WHERE id=$1",
 		id,
 	)
 
 	if err != nil {
 		fmt.Println("Error in deleting event entry: %v", err)
-		// @TODO Log error 
+		// @TODO Log error
 	}
 
 }
